6fdf5b02557647ffa2da440a1c5ed38599f780b7
gaazoo <gaazoo@yandex.ru>
2023-12-12 21:11:05 +0700

    Codding

---
+++ Persister.java
@@ -0,0 +1,9 @@
+public class Persister{
+	private User user;
+	public Persister(User user){
+		this.user = user;
+	}
+	public void save(){
+		System.out.println("Save user: " + user.getName());
+	}
+}
